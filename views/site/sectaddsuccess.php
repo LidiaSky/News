039c26382e8ec17508147a8b73d1f6e365bb4536
Lida Zakharova <zaharova-l@inbox.ru>
2017-02-14 17:33:53 +0300

    "Реализовано добавление секции в БД"

---
+++ views/site/sectaddsuccess.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lidia
+ * Date: 07/02/17
+ * Time: 16:40
+ */
+use yii\helpers\Html;
+?>
+<ui>
+    <li><h2>Секция успешно добавлена!</h2></li>
+    <li><label>Title</label></li>: <?= Html::encode($section->path)?></ui></li>
+<li><label>Abstruct</label></li>: <?= Html::encode($section->description)?></li>
+
+</ui>
+<ui>
+</ui>
