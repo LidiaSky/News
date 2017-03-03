2e949136912ed00bd0fe76323160844a4b15b755
Lida Zakharova <zaharova-l@inbox.ru>
2017-03-03 17:01:37 +0300

    " Есть -  читать статью полностью.  Есть запрос для  реализовать редактирование и удаление новости + вывод по категориям   "

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -89,8 +89,20 @@ class NewsController extends Controller
     public function actionView($id)
     {
         $news = News::find()->where(["id" => $id])->one();
+        $model = new Section();
+        $query =Section::find();
+        $sections = $query->orderBy('name')
+            ->all();
+        $query = Section::find();
+        $sectionview = $query->select(['title','id','nlevel(path) as level'])
+            ->orderby('path')
+            ->all();
 
-        return $this->render('view',['news'=>$news]);
+
+
+
+        return $this->render('view',['news'=>$news,'sections'=> $sections,'model'=> $model,'sectionview'=>$sectionview]);
+
 
     }
 
@@ -109,5 +121,18 @@ class NewsController extends Controller
         return $this->render('index',[
             'sections'=> $sections,'model'=> $model,'sectionview'=>$sectionview
         ]);
+    }
+    public function actionOnesection($id){
+
+        $query = Section::find();
+        $oneSectionNews = $query->select(['title','id','nlevel(path) as level'])
+            ->orderby('path')
+            ->all();
+
+        //Запрос в БД
+        //SELECT * FROM news ns INNER JOIN  (SELECT * FROM news_section WHERE section_id = 25) sec ON ns.id = sec.news_id;
+        SELECT * from news AS n JOIN news_section as ns ON (n.id = ns.news_id) where ns.section_id = 25 ;
+        return $this->render('onesection',['news'=>$oneSectionNews]);
+
     }
 }
